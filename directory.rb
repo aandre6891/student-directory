9e432d8564aee3287075a81521520e3a7923c13b
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-07 08:31:23 +0100

    add titleize method to names

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
+def titleize(string)
+  string.split(" ").map {|word| word.capitalize}.join(" ")
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = titleize(gets.chomp)
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = titleize(gets.chomp)
   end
   # return the array of students
   students
@@ -33,7 +37,6 @@ def print_footer(students)
 end
 
 students = input_students
-#nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
