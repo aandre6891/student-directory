cca635c985525b3a10ab3c9741d91957deb6d216
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-07 08:44:57 +0100

    request more info about students

--- directory.rb
+++ directory.rb
@@ -1,21 +1,32 @@
+# Capitalize every word of a string
 def titleize(string)
   string.split(" ").map {|word| word.capitalize}.join(" ")
 end
 
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
-  # create an empty array
+  # empty array
   students = []
-  # get the first name
+  # get the first name, age and country
   name = titleize(gets.chomp)
+  puts "Please enter the age of this student"
+  age = gets.chomp.to_i
+  puts "Please enter the country of this student"
+  country = titleize(gets.chomp)
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    students << {name: name, age: age, country: country, cohort: :november}
+    puts "Now we have #{students.count} students, add another one or return to end"
+    # get another name, age and country
     name = titleize(gets.chomp)
+    if !name.empty?
+      puts "Please enter the age of this student"
+      age = gets.chomp.to_i
+      puts "Please enter the country of this student"
+      country = titleize(gets.chomp)
+    end
   end
   # return the array of students
   students
@@ -28,7 +39,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index+1}. #{student[:name]} - Age: #{student[:age]} - Country: #{student[:country]} (#{student[:cohort]} cohort)"
   end
 end
 
